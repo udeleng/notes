62ed2977970e1bea0d010bfbfeb0d588fe024d9b
Moshe Solomon <udeleng@gmail.com>
2017-11-15 21:01:19 -0800

    Add enzyme client side testing and PrivateHeader tests

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -6,14 +6,15 @@ const PrivateHeader = (props) => {
         <div className="header">
             <div className="header__content">
                 <h1 className="header__title">{props.title}</h1>
-                <button className="button button--link-text" onClick={() => Accounts.logout()}>Logout</button>
+                <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
             </div>
         </div>
     )
 };
 
 PrivateHeader.propTypes = {
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    handleLogout: React.PropTypes.func.isRequired
 }
 
 export default PrivateHeader;
