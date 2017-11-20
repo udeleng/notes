80d5ea08b05199d7ff42e5331cff10bb2572ad9f
Moshe Solomon <udeleng@gmail.com>
2017-11-19 18:54:01 -0800

    Add Login component test cases

---
+++ imports/ui/Login.test.js
@@ -0,0 +1,52 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import { Login } from './Login';
+import { wrap } from 'module';
+
+if (Meteor.isClient) {
+    describe('Login', function() {
+
+        it('should show error message', function() {
+            const error = 'This is not working';
+            const wrapper = mount(<Login loginWithPassword={() => {}} />);
+
+            wrapper.setState({ error });
+            expect(wrapper.find('p').text()).toBe(error);
+
+            wrapper.setState({ error: '' });
+            expect(wrapper.find('p').length).toBe(0);
+        });
+
+        it('should call loginWithPassword with the form data', function() {
+            const email = 'test@test.com';
+            const password = 'password123';
+            const spy = expect.createSpy();
+            const wrapper = mount(<Login loginWithPassword={spy} />);
+
+            wrapper.ref('email').node.value = email;
+            wrapper.ref('password').node.value = password;
+            wrapper.find('form').simulate('submit');
+
+            expect(spy.calls[0].arguments[0]).toEqual({ email });
+            expect(spy.calls[0].arguments[1]).toBe(password);
+
+        });
+
+        it('should set loginWithPassword callback errors', function() {
+            const spy = expect.createSpy();
+            const wrapper = mount(<Login loginWithPassword={spy} />);
+
+            wrapper.find('form').simulate('submit');
+
+            spy.calls[0].arguments[2]({});
+            expect(wrapper.state('error').length).toNotBe(0);
+
+            spy.calls[0].arguments[2]();
+            expect(wrapper.state('error').length).toBe(0);
+        });
+
+    });
+}
