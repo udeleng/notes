a57a7de48128434118da034c8143357a1fd51a2b
Moshe Solomon <udeleng@gmail.com>
2017-11-30 21:55:21 -0800

    Add NoteListHeader tests

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -13,6 +13,10 @@ export const NoteListHeader = (props) => {
     );
 };
 
+NoteListHeader.propTypes = {
+    meteorCall: Meteor.PropTypes.func.isRequired
+}
+
 export default createContainer(() => {
     return {
         meteorCall: Meteor.call
