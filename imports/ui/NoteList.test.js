d438c1ce5d7caedb743208366e677f9331313dff
Moshe Solomon <udeleng@gmail.com>
2017-12-01 23:11:29 -0800

    Render empty item if no notes

---
+++ imports/ui/NoteList.test.js
@@ -0,0 +1,43 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import { NoteList } from './NoteList';
+
+const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test title',
+        body: '',
+        updatedAt: 0,
+        userId: 'userId1'
+    }, {
+        _id: 'noteId2',
+        title: '',
+        body: 'Something is here',
+        updatedAt: 0,
+        userId: 'userId2'
+    }
+];
+
+if (Meteor.isClient) {
+    describe('NoteList', function() {
+        
+        it('should render note list item for each note', function() {
+            const wrapper = mount( <NoteList notes={notes} /> );
+
+            expect(wrapper.find('NoteListItem').length).toBe(2);
+            expect(wrapper.find('NoteListEmptyItem').length).toBe(0);
+            
+        });
+
+        it('should render note list empty item of zero notes', function() {
+            const wrapper = mount( <NoteList notes={[]} /> );
+            
+            expect(wrapper.find('NoteListItem').length).toBe(0);
+            expect(wrapper.find('NoteListEmptyItem').length).toBe(1);
+        });
+
+    });
+}
--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -14,7 +14,7 @@ export const NoteListHeader = (props) => {
 };
 
 NoteListHeader.propTypes = {
-    meteorCall: Meteor.PropTypes.func.isRequired
+    meteorCall: React.PropTypes.func.isRequired
 }
 
 export default createContainer(() => {
