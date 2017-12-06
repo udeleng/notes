90d3fe128f4f731dee7a2023246a4b3579d3e9cb
Moshe Solomon <udeleng@gmail.com>
2017-12-05 21:34:28 -0800

    Save selected note id in session and update url

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import ReactDom from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import { browserHistory } from 'react-router';
 
 import {routes, onAuthChange} from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration';
@@ -11,6 +12,15 @@ Tracker.autorun(() => {
   onAuthChange(isAuthenticated);
 });
 
+Tracker.autorun(() => {
+  const selectedNodeId = Session.get('selectedNodeId');
+
+  if (selectedNodeId) {
+    browserHistory.replace(`/dashboard/${selectedNodeId}`);
+  }
+});
+
 Meteor.startup(() => {
+  Session.set('selectedNodeId', undefined);
   ReactDom.render(routes, document.getElementById('app'));
 });
