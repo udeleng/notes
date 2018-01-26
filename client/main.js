c91aa70020a4feae0c6e4a639f4c56c1ebac8cf3
Moshe Solomon <udeleng@gmail.com>
2018-01-25 21:50:12 -0800

    Setup mobile navigation

--- client/main.js
+++ client/main.js
@@ -16,13 +16,20 @@ Tracker.autorun(() => {
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
+  Session.set('isNavOpen', false);
 
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
   }
 });
 
+Tracker.autorun(() => {
+  const isNavOpen = Session.get('isNavOpen');
+  document.body.classList.toggle('is-nav-open', isNavOpen);
+});
+
 Meteor.startup(() => {
   Session.set('selectedNoteId', undefined);
+  Session.set("isNavOpen", false);
   ReactDom.render(routes, document.getElementById('app'));
 });
